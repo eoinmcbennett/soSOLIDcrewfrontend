abf8d8fde9e38a8fb6c62ac375e93ac668664ec9
eoinmcbennett <eoinmcbennett12345@gmail.com>
2023-10-26 14:48:00 +0100

    Delete delivery employee functionality

--- controller/deliveryEmployeeController.ts
+++ controller/deliveryEmployeeController.ts
@@ -19,6 +19,23 @@ module.exports = function(app: Application){
         res.send(data);
     })
 
+    app.get('/delete-delivery-employee', async(req: Request, res:Response) => {
+        res.render('delete-delivery-employee',{ deliveryEmployees: await deliveryEmployeeService.getDeliveryEmployees(req.session.token) })
+    })
+
+    app.post("/delete-delivery-employee",async(req: Request,res:Response) => {
+        let employeeId: number = req.body.employeeId;
+        try {
+            await deliveryEmployeeService.deleteDeliveryEmployee(employeeId,req.session.token);
+            res.locals.successmessage = "Sucessfully Deleted Delivery Employee"
+
+            res.render("delete-delivery-employee",{ deliveryEmployees: await deliveryEmployeeService.getDeliveryEmployees(req.session.token) })
+        } catch(e){
+            res.locals.errormessage = e.message
+            res.render("delete-delivery-employee",{ deliveryEmployees: await deliveryEmployeeService.getDeliveryEmployees(req.session.token) })
+        }
+    })
+
 }
 
 
--- service/deliveryEmployeeService.ts
+++ service/deliveryEmployeeService.ts
@@ -1,14 +1,23 @@
 
 const axios = require('axios');
 
- module.exports.getDeliveryEmployees = async function (){
+ module.exports.getDeliveryEmployees = async function (token:string){
     try{
-        const response = await axios.get('http://localhost:8080/api/employees/delivery')
+        const response = await axios.get('http://localhost:8080/api/employees/delivery',{ params: { token: token  } })
 
-        console.log(response)
         return response.data
     }catch(e){
         return new Error('Could not get Delivery Employees')
     }
 
+}
+
+module.exports.deleteDeliveryEmployee = async function(id:number,token:string) : Promise<boolean> {
+    console.log(id)
+    try {
+        const response = await axios.delete("http://localhost:8080/api/employees/delivery/" + id,{ params: { token: token  } })
+        return response.status == 200
+    } catch(e){
+        throw new Error(e.message);
+    }
 }
