5e061f5400ab89b75a09359b3c301ba0c6c59436
Stephen Hanna <stephenhanna@c02gf0b9ml85.client.kainos.com>
2023-10-26 14:16:13 +0100

    0003 show delivert employee implemented

--- service/deliveryEmployeeService.ts
+++ service/deliveryEmployeeService.ts
@@ -16,7 +16,10 @@ const axios = require('axios');
 
 module.exports.getDeliveryEmployee = async function(id: string, token: string): Promise<DeliveryEmployee> {
     try {
-        const response = await axios.get('http://localhost:8080/api/employees/delivery/'+id, null, {token: token})
+
+        console.log(token)
+
+        const response = await axios.get('http://localhost:8080/api/employees/delivery/'+id, { params: {token: token} })
 
         return response.data
     } catch {
