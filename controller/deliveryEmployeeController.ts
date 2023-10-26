d25950397829cb261ef12ca41016716b4c13da0d
Stephen Hanna <stephenhanna@c02gf0b9ml85.client.kainos.com>
2023-10-26 14:32:05 +0100

    WIP

--- controller/deliveryEmployeeController.ts
+++ controller/deliveryEmployeeController.ts
@@ -19,6 +19,21 @@ module.exports = function(app: Application){
         res.send(data);
     })
 
+
+    app.get('/updatedeliveryemployee', async(req: Request,res:Response) =>{
+        let data: DeliveryEmployee;
+
+        try {
+            data = await deliveryEmployeeService.getDeliveryEmployees()
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+        console.log("Hello world");
+
+        res.send(data);
+    })
+
 }
 
 
